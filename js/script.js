a2865902aa3d76485f6f3f43919ba3b7dc78e293
Maksim_SHelyuto <shelyuto_max@mail.ru>
2023-01-16 18:57:36 +0300

    add: icons, logic for moving on page

--- js/script.js
+++ js/script.js
@@ -1,11 +1,42 @@
 let sidebarOpen = false;
+let btn;
+let sidebar;
 
 function windowResizeAction() {
     document.documentElement.style.setProperty('--bodyWidth', `${window.innerWidth}px`);
 }
 
+function sideBarAction() {
+
+    let mouseInSideBar = false;
+    sidebar.addEventListener('mouseenter', (e) => mouseInSideBar = true);
+    sidebar.addEventListener('mouseleave', (e) => mouseInSideBar = false);
+    btn.addEventListener("click", (e) => toggleSideBar());
+    btn.addEventListener('focusout', (e) => {
+        if (mouseInSideBar) {
+            btn.focus();
+        } else {
+            sidebarOpen = false;
+            sidebar.classList.remove('main__col-placeholder__opened');
+            btn.classList.remove('main__menu-action_open');
+        }
+    });
+}
+
+function toggleSideBar(){
+    sidebarOpen = !sidebarOpen;
+    sidebarOpen ? btn.classList.add('main__menu-action_open') : btn.classList.remove('main__menu-action_open');
+    sidebarOpen ? sidebar.classList.add('main__col-placeholder__opened') : sidebar.classList.remove('main__col-placeholder__opened');
+    const navItems = document.getElementsByClassName('main__col-placeholder__nav-item');
+    for (let navItem of navItems) {
+        sidebarOpen ? navItem.classList.remove('main__col-placeholder__nav-item--hidden') : navItem.classList.add('main__col-placeholder__nav-item--hidden');
+    }
+}
+
 window.addEventListener("load", (event) => {
     console.log("page is fully loaded");
+    btn = document.getElementById('sidebar_action');
+    sidebar = document.getElementById('sidebar');
     windowResizeAction();
     sideBarAction();
     window.addEventListener("resize", windowResizeAction);
@@ -21,27 +52,18 @@ window.addEventListener("load", (event) => {
     }
     // spinner.classList.remove('main__loader__hided');
     setTimeout(() => document.getElementById('spinner').remove(), 5500);
+    const navItems = document.getElementsByClassName('dynamic-link');
+    for (let navItem of navItems) {
+        navItem.addEventListener('click', (e) => {
+            e.preventDefault();
+            if(sidebarOpen){
+                toggleSideBar();
+            }
+            document.querySelector(navItem.getAttribute('data-link'))
+                .scrollIntoView({behavior: 'smooth', block: 'start'});
+        });
+    }
 });
 
-function sideBarAction() {
-    const btn = document.getElementById('sidebar_action');
-    const sidebar = document.getElementById('sidebar');
-    let mouseInSideBar = false;
-    sidebar.addEventListener('mouseenter', (e) => mouseInSideBar = true);
-    sidebar.addEventListener('mouseleave', (e) => mouseInSideBar = false);
-    btn.addEventListener("click", (e) => {
-        sidebarOpen = !sidebarOpen;
-        sidebarOpen ? btn.classList.add('main__menu-action_open') : btn.classList.remove('main__menu-action_open');
-        sidebarOpen ? sidebar.classList.add('main__col-placeholder__opened') : sidebar.classList.remove('main__col-placeholder__opened');
-    });
-    btn.addEventListener('focusout', (e) => {
-        if (mouseInSideBar) {
-            btn.focus();
-        } else {
-            sidebarOpen = false;
-            sidebar.classList.remove('main__col-placeholder__opened');
-            btn.classList.remove('main__menu-action_open');
-        }
-    });
-}
+
 
